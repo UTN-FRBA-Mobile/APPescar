1a5f48e4443d04477d13c259436ee98151399495
sebaperez <sperez@est.frba.utn.edu.ar>
2017-06-30 20:10:38 -0300

    Se obtiene imagen que fue cargada desde la galeria

--- app/src/main/java/com/appescar/appescar/FormAdd.java
+++ app/src/main/java/com/appescar/appescar/FormAdd.java
@@ -1,12 +1,16 @@
 package com.appescar.appescar;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FormAdd extends AppCompatActivity {
 
@@ -21,9 +25,9 @@ public class FormAdd extends AppCompatActivity {
 
         Button uploadImageButton = (Button)findViewById(R.id.uploadImageButton);
         uploadImageButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -31,7 +35,21 @@ public class FormAdd extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        FormAdd.super.onActivityResult(requestCode, resultCode, data);
 
-
-
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeFile(selectedImage.toString(), options);
+                ImageView imageUploadPreview = (ImageView) findViewById(R.id.imageUploadPreview);
+                imageUploadPreview.setImageBitmap(bitmap);
+            } else {
+                // Mostrar error - No se pudo obtener la imagen
+            }
+        }
+    }
 }
