a32ae26abab3608301f064fb23bb1bd0810eec44
sebaperez <sperez@est.frba.utn.edu.ar>
2017-06-30 19:24:20 -0300

    Abrir galeria para seleccionar imagen

--- app/src/main/java/com/appescar/appescar/FormAdd.java
+++ app/src/main/java/com/appescar/appescar/FormAdd.java
@@ -2,29 +2,36 @@ package com.appescar.appescar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class FormAdd extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_add);
-     //   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-     //   setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button uploadImageButton = (Button)findViewById(R.id.uploadImageButton);
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "@string/pick_photo"), PICK_IMAGE);
             }
-        }); */
+        });
     }
+
+
 
 }
