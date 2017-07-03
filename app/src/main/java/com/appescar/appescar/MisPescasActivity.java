72d04d1eebb8790f6b03d1c1fc7bb6cb4c906cf3
demianabiusi <demian@abiusi.com.ar>
2017-07-03 11:55:55 -0300

    UID filter on listview MyFishing

--- app/src/main/java/com/appescar/appescar/MisPescasActivity.java
+++ app/src/main/java/com/appescar/appescar/MisPescasActivity.java
@@ -1,5 +1,7 @@
 package com.appescar.appescar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +18,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 
 public class MisPescasActivity extends AppCompatActivity {
 
@@ -33,10 +40,13 @@ public class MisPescasActivity extends AppCompatActivity {
 
         refDatabase = FirebaseDatabase.getInstance().getReference().child("pescas");
 
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+
+
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.lv_mispescas);
 
-        ListAdapter adapter = new FirebaseListAdapter<Pesca>(this, Pesca.class, R.layout.una_pesca, refDatabase) {
+        final ListAdapter adapter = new FirebaseListAdapter<Pesca>(this, Pesca.class, R.layout.una_pesca, refDatabase.orderByChild("uid").equalTo(currentFirebaseUser.getUid())) {
             @Override
             protected void populateView(View view, Pesca pesca, int position) {
 
@@ -63,6 +73,37 @@ public class MisPescasActivity extends AppCompatActivity {
         };
 
 
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id)
+            {
+               // String selectedFromList = (listView.getItemAtPosition(position).getString());
+
+                Pesca pesca = (Pesca) adapter.getItem(position);
+
+                Intent intent = new Intent(MisPescasActivity.this, DetallesPesca.class);
+                intent.putExtra("fish",pesca.getFish());
+                intent.putExtra("line",pesca.getLine());
+                intent.putExtra("bait",pesca.getBait());
+
+                byte[] decoded = Base64.decode(pesca.getImg(), Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+                String fileName = "myImage";//no .png or .jpg needed
+                try {
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    decodedByte.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                    FileOutputStream fo = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    fo.write(bytes.toByteArray());
+                    // remember close file output
+                    fo.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                intent.putExtra("img",fileName);
+                intent.putExtra("description",pesca.getDescription());
+                startActivity(intent);
+
+            }});
 
 
         listView.setAdapter(adapter);
